76ac0547eef7987fa187a6b6485660938c300f1c
47818522 <fardiz.khan@students.mq.edu.au>
2024-08-07 16:35:57 +1000

    Started task 7

---
+++ Bird.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Bird extends Actor{
+
+    public Bird(int inX, int inY) {
+        super(inX, inY);
+    }
+    
+    public void paint(Graphics g){
+        
+    }
+}
---
+++ src/Actor.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Actor {
+    
+
+    public Actor(int inX, int inY){
+        Cell location = new Cell(inX, inY);
+    }
+
+   
+}
