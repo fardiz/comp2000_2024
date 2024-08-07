538d04156beaf0d60ef51b07cdfe5789376137d1
47818522 <fardiz.khan@students.mq.edu.au>
2024-08-07 16:41:17 +1000

    Attempted task 7

---
+++ Cat.java
@@ -0,0 +1,7 @@
+public class Cat extends Actor {
+
+    public Cat(int inX, int inY) {
+        super(inX, inY);
+    }
+    
+}
---
+++ src/Stage.java
@@ -0,0 +1,7 @@
+public class Stage {
+    Grid grid = new Grid();
+
+    public Stage(){
+        
+    }
+}
