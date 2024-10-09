da7220e8f2959637c8133ea1b2f808d11e34daf3
47818522 <fardiz.khan@students.mq.edu.au>
2024-10-09 16:10:29 +1100

    Task 21 completed.

--- src/LeftMostMove.java
+++ src/LeftMostMove.java
@@ -4,11 +4,10 @@ public class LeftMostMove implements MoveStrategy {
   @Override
   public Cell chooseNextLoc(List<Cell> possibleLocs) {
     Cell currLM = possibleLocs.get(0);
-    for(Cell c: possibleLocs) {
-      if(c.leftOfComparison(currLM) < 0) {
-        currLM = c;
-      }
-    }
+    final Cell currLM2 = currLM;
+    currLM = possibleLocs.stream().filter(c -> c.leftOfComparison(currLM2) < 0)
+    .reduce(currLM, (c1, c2) -> c2.leftOfComparison(c1) < 0 ? c2 : c1);
+
     return currLM;
   }
 
